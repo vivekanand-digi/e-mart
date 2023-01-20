7943bb42c53461f057a0dc04ad7061f6b3e2d829
mainivivek <115615231+mainivivek@users.noreply.github.com>
2023-01-20 12:01:57 +0530

    firstCommit

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {NavLink } from "react-router-dom";
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm ">
+        <NavLink className="navbar-brand fw-bold fs-4" to="/">Navbar</NavLink>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+       LA COLLECTION 
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mx-auto">
+            <li className="nav-item active">
+              <NavLink className="nav-link" to="/">Home <span className="sr-only">(current)</span></NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/products">Products</NavLink>
+            </li>
+
+        
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">About</NavLink>
+            </li>
+       
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/contact">Contact</NavLink>
+            </li>
+         
+          </ul>
+          <div className="buttons">
+            <NavLink to="/cart" className="btn btn-outline-dark ms-2">
+              <i className="fa fa-shopping-cart me-1"></i>Cart (0)</NavLink>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+export default Navbar;
